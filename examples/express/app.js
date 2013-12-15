2c6c6bb3f2b19c9c53b1325fe2bd83190e8d0e95
Shuvalov Anton <anton_shuvalov@me.com>
2013-12-15 21:26:51 +0600

    Respawn children if exit

--- examples/express/app.js
+++ examples/express/app.js
@@ -33,3 +33,7 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+setTimeout(function () {
+  process.exit();
+}, 10000);
